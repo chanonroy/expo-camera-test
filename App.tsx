45e73dc59df8a71dbc89b8548651571502ebbb29
Chanon Roy <chanonroy@gmail.com>
2021-06-18 11:37:19 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const cameraRef = useRef<Camera>(null);
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const handleCaptureImage = async () => {
+    if (!cameraRef.current) return;
+    console.log('capture');
+    // Take a picture with the camera
+    const result = await cameraRef.current.takePictureAsync({
+      base64: true,
+      quality: 1,
+    });
+    console.log(result);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Camera ref={cameraRef} style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleCaptureImage}>
+            <Text style={styles.text}> CAPTURE </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -12,8 +49,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
